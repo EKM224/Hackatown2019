625e6ae241b292e65e0f9205ba7e216f86eb3f94
Evan Kirby McGregor <evan.km@hotmail.com>
2019-01-19 23:26:43 -0500

    EKM: D added input boxes and console log

--- start/myapp/src/app/home/home.page.ts
+++ start/myapp/src/app/home/home.page.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  inLat: number;
+  inLong: number;
+  inType: string;
+  inKey: string;
+  inRad: number;
+
+  constructor(public mapsSerice: PlacesAPIService){
+
+  }
+  loadPlaces(){
+    let places = this.mapsSerice.getPlaces(this.inLat, this.inLong, this.inType, this.inKey, this.inRad)
+    console.log(places);
+  }
 
 }
