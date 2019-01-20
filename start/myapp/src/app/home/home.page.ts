9d84aff133d5fcb9789c954d69201f52cf719e42
William Benoit <wilberben@hotmail.com>
2019-01-20 07:13:41 -0500

    WB D UI

--- start/myapp/src/app/home/home.page.ts
+++ start/myapp/src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { GeoService } from './../geo.service';
 import { PlacesAPIService } from './../PlacesAPI/places-api.service';
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Lieu } from '../Lieu';
 
 @Component({
@@ -9,27 +9,37 @@ import { Lieu } from '../Lieu';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  inLat: number; //= 45.502990;
+  @ViewChild('home') home: ElementRef;
+  @ViewChild('result') result: ElementRef;
+  inLat: number; // = 45.502990;
   inLong: number; // = -73.613990;
-  inType: string = "restaurant";
+  inType = 'restaurant';
   inKey: string;
   inRad = 1500;
+  endroits: Lieu[];
 
   constructor(public mapsSerice: PlacesAPIService, public geoService: GeoService){
-
   }
 
-  useMyLocation(){
+  useMyLocation() {
     this.geoService.getLocation();
     this.inLat = this.geoService.lat;
     this.inLong = this.geoService.long;
   }
 
-  loadPlaces(){
+  loadPlaces() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('result').style.display = 'block';
     let places: Lieu[] = new Array();
     places = this.mapsSerice.getPlaces(this.inLat, this.inLong, this.inType, this.inKey, this.inRad);
     console.log(places);
+    this.endroits = places;
     console.log(places.length);
     this.mapsSerice.getDirection(places[0].adresse, places[1].adresse);
+  }
+
+  annuler() {
+      document.getElementById('result').style.display = 'none';
+      document.getElementById('home').style.display = 'block';
   }
 }
