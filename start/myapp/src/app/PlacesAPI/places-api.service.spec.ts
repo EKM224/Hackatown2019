de47e75c99750c11dee8faa4aff8e123e036c39d
Alexandre <alexandre.h.falardeau@gmail.com>
2019-01-19 19:53:51 -0500

    API stuff

--- start/myapp/src/app/PlacesAPI/places-api.service.spec.ts
+++ start/myapp/src/app/PlacesAPI/places-api.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { PlacesAPIService } from './places-api.service';
 
 describe('PlacesAPIService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: PlacesAPIService = TestBed.get(PlacesAPIService);
-    expect(service).toBeTruthy();
+      const service: PlacesAPIService = TestBed.get(PlacesAPIService);
+      expect(service).toBeTruthy();
+  });
+
+  it('should get some specific data', () => {
+    expect(1).toBe(1);
   });
 });
--- start/myapp/src/app/PlacesAPI/places-api.service.ts
+++ start/myapp/src/app/PlacesAPI/places-api.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesAPIService {
+  private placesUrl: string;
+  private googleAPIKey: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.placesUrl = "AIzaSyCqS8bXAYAOrObOu6-eWUE0mCsw0tKBKMY"
+    this.placesUrl = 'https://maps.googleapis.com/maps/api/place/' +
+    'nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&' +
+    'type=restaurant&keyword=cruise&key=AIzaSyCqS8bXAYAOrObOu6-eWUE0mCsw0tKBKMY';
+   }
+
+  testClient() {
+   return null;
+  }
+
+  getPlaces(): Observable<any> {
+    return this.http.get<any>(this.placesUrl);
+  }
 }
